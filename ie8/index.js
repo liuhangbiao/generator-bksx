8b1b879700989ed9800dbb40a4ea7229cfff9c87
Yeal Zoy <314186096@qq.com>
2017-03-23 17:49:44 +0800

    yosay修改好了

--- ie8/index.js
+++ ie8/index.js
@@ -1,5 +1,5 @@
 var Generator = require('yeoman-generator');
-//var yosay = require('yosay');
+var yosay = require('yosay');
 var chalk = require('chalk'); 
 module.exports = class extends Generator{
 	constructor(args, opts) {
@@ -7,7 +7,7 @@ module.exports = class extends Generator{
 	}
 	initializing(){
 		//初始化
-		//this.log(yosay('北控三兴web前端欢迎您！\n congratulations,\n you\'re going to bulid a webProject, \n the best wishes for you \n no bug!'));
+		this.log(yosay('北控三兴web前端欢迎您！\n congratulations,\n you\'re going to bulid a webProject, \n the best wishes for you \n no bug!'));
 		this.log(chalk.magenta('ie8'));
 	}
 }
